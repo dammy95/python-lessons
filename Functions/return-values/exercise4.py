a57f9641ccd0146bd322ffd4e8b1180d36fc15ab
dammy95 <damilola@dabapps.com>
2017-11-24 12:53:37 +0000

    Recursive Functions - exercise4.py

---
+++ Functions/return-values/exercise4.py
@@ -0,0 +1,11 @@
+
+def rec_factorial(n):
+  if n < 2:
+    return 1
+
+  return n * rec_factorial(n - 1)
+
+if __name__ == '__main__':
+  print(rec_factorial(3))
+  print(rec_factorial(4))
+  print(rec_factorial(6))
